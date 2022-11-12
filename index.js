edfa37f3107de6a0fa1c1001ccca8990169ef537
Daniel Clements <drclements@liberty.edu>
2022-11-12 15:06:48 -0500

    commit tree bc5fe8b05e9297ff02a1808ef50385e27d0a5e31

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // look through each item in the array and assign variable num1
+  for ( let i = 0; i < array.length; i ++) {
+    let num1 = array[i]
+    //look through the rest of the array and assign variable num2
+    for ( let j = i +1; j < array.length; j++ ) {
+      let num2 = array[j]
+    // add each number in the array
+    if (num1 + num2 === target) {
+      return true
+    }
+    }
+  }
+return false
+
 }
 
 /* 
@@ -7,7 +20,9 @@ function hasTargetSum(array, target) {
 */
 
 /* 
-  Add your pseudocode here
+  iterate over the array starting with the start and adding the second, then thrid and so on.
+  if num1 + num2 = target number
+  return true 
 */
 
 /*
@@ -19,6 +34,11 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([22, 19, 8, 30, 4, 4, 6, 10], 32));
 
   console.log("");
 
